7a9455211918874943b36b72de0dd366e5e48dd9
Zeque G-M <zequezz99@gmail.com>
2019-01-25 13:57:21 -0800

    Added v1 follow player AI
    Only kind of buggy, noticed jitter bug in player movement, going to revert

---
+++ Assets/Scripts/NPCPlatformerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCPlatformerController : PhysicsObject
+{
+    public float maxSpeed = 5f;
+
+    protected Vector2 targetDirection;
+
+    protected override void ComputeVelocity()
+    {
+        FindTargetDirection();
+        targetVelocity = targetDirection.normalized * maxSpeed;
+    }
+
+    protected virtual void FindTargetDirection() { }
+}
---
+++ Assets/Scripts/XFollowPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XFollowPlayer : NPCPlatformerController
+{
+    public float minFollowDistance = 0.5f;
+    public float maxFollowDistance = 2f;
+
+    private float targetX;
+
+    private const float xThreshold = 0.1f;
+
+    protected override void FindTargetDirection()
+    {
+        float x = transform.position.x;
+        float playerX = FindObjectOfType<PlayerPlatformerController>().transform.position.x;
+
+        if ((Mathf.Abs(targetX - playerX) > maxFollowDistance) /*&& (Mathf.Abs(targetX - x) <= xThreshold)*/)
+        {
+            if (playerX > x)
+                targetX = playerX - Random.Range(minFollowDistance, maxFollowDistance);
+            else
+                targetX = playerX + Random.Range(minFollowDistance, maxFollowDistance);
+        }
+
+        if (Mathf.Abs(targetX - x) > xThreshold)
+        {
+            targetDirection = new Vector2(targetX - x, 0);
+        }
+        else
+        {
+            targetDirection = Vector2.zero;
+        }
+    }
+
+    private void Awake()
+    {
+        targetX = transform.position.x;
+    }
+}
